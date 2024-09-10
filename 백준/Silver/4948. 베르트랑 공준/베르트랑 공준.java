0b032af8d4b31df41bf59f22bb2741669d117e38
ssem-i <115564717+ssem-i@users.noreply.github.com>
2024-09-10 23:11:18 +0900

    [Silver II] Title: 베르트랑 공준, Time: 256 ms, Memory: 21320 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -3,25 +3,23 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		while(true) {
-			int n = sc.nextInt();
-			
-			if(n==0) break;
-			if(n==1) {
-				System.out.println("1");
-				continue;
-			}
-			
-			int count=0;
-			boolean[] isPrime = new boolean[2*n+1];
-			Arrays.fill(isPrime, true);
-			for(int i=2; i*i<=2*n; i++) {
-				if(isPrime[i]) {
-					for(int k=i*i;k<=2*n;k+=i) {
-						isPrime[k]=false;
-					}
+		int MAX = 123456*2;
+		boolean[] isPrime = new boolean[MAX+1];
+		Arrays.fill(isPrime, true);
+		isPrime[0] = isPrime[1] = false;
+		for(int i=2; i*i<=MAX; i++) {
+			if(isPrime[i]) {
+				for(int k=i*i;k<=MAX;k+=i) {
+					isPrime[k]=false;
 				}
 			}
+		}
+		
+		while(true) {
+			int n = sc.nextInt();	
+			if(n==0) break;
+			int count=0;
+			
 			for (int i = n+1; i <= 2*n; i++) {
 	            if (isPrime[i]) {
 	                count++;
